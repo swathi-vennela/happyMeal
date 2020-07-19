3b6c28f26a9228c3c7cbb4be7c297de844eca27e
Nikhil Kataria <44164498+katarianikhilk@users.noreply.github.com>
2020-07-19 20:42:56 +0530

    Revert "chef dashboard with some features"

--- NewDjango/core/admin.py
+++ NewDjango/core/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from .models import Item, OrderItem, Order, Review, OrderedList
+from .models import Item, OrderItem, Order, Review
 
 
 admin.site.register(Item)
 admin.site.register(OrderItem)
 admin.site.register(Order)
 admin.site.register(Review)
-admin.site.register(OrderedList)
 
